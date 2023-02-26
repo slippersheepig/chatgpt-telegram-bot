ac00313e158b03cb70399d4557ae4a6acbc6d8d9
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-02-26 17:10:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from revChatGPT.Official import AsyncChatbot as ChatGPT3Bot
+from revChatGPT.V1 import AsyncChatbot as ChatGPT3Bot
 
 from telegram_bot import ChatGPT3TelegramBot
 
@@ -18,7 +18,7 @@ def main():
     )
 
     # Check if the required environment variables are set
-    required_values = ['TELEGRAM_BOT_TOKEN', 'OPENAI_API_KEY']
+    required_values = ['TELEGRAM_BOT_TOKEN', 'OPENAI_EMAIL', 'OPENAI_PASSWORD']
     missing_values = [value for value in required_values if os.environ.get(value) is None]
     if len(missing_values) > 0:
         logging.error(f'The following environment values are missing in your .env: {", ".join(missing_values)}')
@@ -26,7 +26,8 @@ def main():
 
     # Setup configuration
     chatgpt_config = {
-        'api_key': os.environ['OPENAI_API_KEY']
+        'email': os.environ['OPENAI_EMAIL'],
+        'password': os.environ['OPENAI_PASSWORD']
     }
     telegram_config = {
         'token': os.environ['TELEGRAM_BOT_TOKEN'],
@@ -34,7 +35,7 @@ def main():
     }
 
     # Setup and run ChatGPT and Telegram bot
-    gpt3_bot = ChatGPT3Bot(api_key=chatgpt_config['api_key'])
+    gpt3_bot = ChatGPT3Bot(config=chatgpt_config)
     telegram_bot = ChatGPT3TelegramBot(config=telegram_config, gpt3_bot=gpt3_bot)
     telegram_bot.run()
 
