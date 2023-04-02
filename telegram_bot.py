32b47a929a27ad078f31c2773eafbf9d38ac7513
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-04-02 14:02:41 +0800

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -173,7 +173,7 @@ class ChatGPT3TelegramBot:
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
                 reply_to_message_id=update.message.message_id,
-                text=response,
+                text=response['message'],
                 parse_mode=constants.ParseMode.MARKDOWN
             )
 
@@ -189,7 +189,7 @@ class ChatGPT3TelegramBot:
         except Exception as e:
             error_text = f'Error while getting the response: {str(e)}'
             logging.info(error_text)
-            return error_text
+            return { "message": "error_text" }
 
     async def send_disallowed_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
